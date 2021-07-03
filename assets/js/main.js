332f49b95f4eb489ae76000260f4b4623aabc519
diegozenteno <d.zenteno01@ufromail.cl>
2021-07-03 04:09:42 +0000

    tooltips

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('.carousel').carousel({
-        interval: 4000
-      })
+  $('.carousel').carousel({
+    interval: 4000
+  })
+  // Evento click
+  $("a").click(function (event) {
+    event.preventDefault();
+
+    // SmoothScroll - variable gato guarda los id
+    let gato = this.hash;
+    $("html").animate({
+      scrollTop: $(gato).offset().top,
+    },
+      800)
+  })
+
+  // tooltip
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
 
 
 })
